32b1621ba794f9b6c6998c05efce4e1439b3caff
Pawel-9215 <hordyniak@gmail.com>
2021-06-14 23:49:47 +0200

    base logic for driving is working

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 from machine import Pin, PWM
 import utime
 
+
+
 frontWheel_servo = PWM(Pin(0))
 frontWheel_servo.freq(50)
 
@@ -19,24 +21,64 @@ Lets figure that out - duty
 value = 1350 + Xdeg * (6850/180)
 """
 
-def move_forward():
+def move_backward():
     backwheel_dir_back.value(0)
     backwheel_dir_for.value(1)
     
-def move_backward():
+def move_forward():
     backwheel_dir_for.value(0)
     backwheel_dir_back.value(1)
+    
+def set_speed(speed):
+    backwheel_pwm.duty_u16(int(speed*speed))
+    
+def turn_left():
+    direction_fw = int(1350 + 45 * (6858/180))
+    frontWheel_servo.duty_u16(direction_fw)
+    move_forward()
+    set_speed(180)
+    utime.sleep(2)
+    set_speed(0)
+    
+def turn_right():
+    direction_fw = int(1350 + (90+45) * (6858/180))
+    frontWheel_servo.duty_u16(direction_fw)
+    move_forward()
+    set_speed(180)
+    utime.sleep(2)
+    set_speed(0)
+    
+def reset_front():
+    direction_fw = int(1350 + 90 * (6858/180))
+    frontWheel_servo.duty_u16(direction_fw)
+    
 
 while True:
     #direction = int(1350 + int(input()) * (6858/180))
     #frontWheel_servo.duty_u16(direction)
     dir12 = input()
+    
     if dir12 == "w":
+        reset_front()
         move_forward()
-    elif dir12 == "s":
-        move_backward()
+        set_speed(160)
+        utime.sleep(2)
+        set_speed(0)
         
-    else:
-        direction = int(dir12)
-        backwheel_pwm.duty_u16(int(direction*direction))
-        utime.sleep(0.02)
+    elif dir12 == "s":
+        reset_front()
+        move_backward()
+        set_speed(160)
+        utime.sleep(2)
+        set_speed(0)
+    
+    elif dir12 == "a":
+        turn_left()
+        
+    elif dir12 == "d":
+        turn_right()
+        
+
+
+
+
